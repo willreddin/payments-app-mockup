0c066e43d926390e509be690a340de16484c00f6
William Reddin <willreddin88@gmail.com>
2016-01-30 14:38:10 +0000

    removed reference to keys in application.yml and but in the keys manually to diagnose publishabel key problem

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => 'pk_test_DdqMhMAi357xLxM6BsKfOTpv'
+  :secret_key      => 'sk_test_t2EXHTR4RcPhCqVq6alT2MJN'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
